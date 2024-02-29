f3f10681e57935be40cb5c03e60cfc64346c0f21
Abhay kumawat <abhaykumawat81@gmail.com>
2024-02-29 20:30:59 +0530

    Fix Some Error

--- src/App.js
+++ src/App.js
@@ -4,24 +4,9 @@ import Weather from "./Components/WEATHER/Weather";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import NightlightIcon from "@mui/icons-material/Nightlight";
 function App() {
-  const lightmod = () => {
-    const lightMode = (document.body.style.backgroundColor = "white");
-    document.getElementById("setcolor").style.color = "black";
-  };
-  const darkmod = () => {
-    document.body.style.backgroundColor = "black";
-    document.getElementById("setcolor").style.color = "white";
-  };
   return (
     <>
-      <div style={{textAlign:"end",marginRight:"5%"
-      }}>
-        <LightModeIcon onClick={lightmod} />
-
-        <NightlightIcon onClick={darkmod} />
-      </div>
       <div className="App">
-        <h1 id="setcolor">Check Weather In Your City</h1>
         <Weather />
       </div>
     </>
--- src/Components/WEATHER/Weather.js
+++ src/Components/WEATHER/Weather.js
@@ -1,59 +1,72 @@
-import React from "react";
-import "./Style.css";
-import styled from "@emotion/styled";
-import { useEffect } from "react";
-import { TextField, Button } from "@mui/material";
-import { DataGrid, rows, columns } from "@mui/x-data-grid";
+    import React from "react";
+    import "./Style.css";
+    import styled from "@emotion/styled";
+    import { useEffect } from "react";
+    import { TextField, Button } from "@mui/material";
+    import { DataGrid, rows, columns } from "@mui/x-data-grid";
 
-const Text = styled(TextField)`
-  color: white;
+    const Text = styled(TextField)`
+      color: white;
 
-  width: 100%;
-`;
-function Weather() {
-  const [city, setCity] = React.useState(null);
-  const [search, setSearch] = React.useState("");
+      width: 100%;
+    `;
+    function Weather() {
+      const [city, setCity] = React.useState(null);
+      const [search, setSearch] = React.useState("");
 
-  useEffect(() => {
-    const fetchAppi = async () => {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=2895d35387429c2bf7e4a59bef718c13`;
-      const response = await fetch(url);
-      const resJson = await response.json();
-      setCity(resJson.main);
-    };
-    fetchAppi();
-  }, [search]);
+      useEffect(() => {
+        const fetchAppi = async () => {
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=2895d35387429c2bf7e4a59bef718c13`;
+          const response = await fetch(url);
+          const resJson = await response.json();
+          setCity(resJson.main);
+        };
+        fetchAppi();
+      }, [search]);
 
-  const handlchange = (event) => {
-    event.preventDefault();
-    setSearch(event.target.value);
-  };
+      const handlchange = (event) => {
+        event.preventDefault(); 
+        setSearch(event.target.value);
+      };
 
-  return (
-    <>
-      <div className="container">
-        <div className="Box">
-          <h1>Weather App</h1>
-          <Text
-            id="standard-basic"
-            label="Enter City"
-            variant="standard"
-            onChange={handlchange}
-          />
+      return (
+        <>
+          <h1 style={{ color: "white" }}>
+            Check Weather In<span > Your City </span>{" "}
+          </h1>
+          <div className="container">
+            <div className="Box">
+              <h1 style={{ color: "black" }}>
+                Weather <span>App</span>
+              </h1>
+              <Text
+                style={{ color: "white" }}
+                id="standard-basic"
+                label="Enter City"
+                variant="standard"
+                onChange={handlchange}
+              />
 
-          {!city ? (
-            <p> No data found</p>
-          ) : (
-            <div>
-              <h1>{search}</h1>
-              <h2> {city.temp}<span >&#8451;</span></h2>
-                <p>Min-temp : {city.temp_min}<span>&#37;</span> || Max-temp :{ city.temp_max}<span>&#37;</span></p>
+              {!city ? (
+                <p style={{color:"white"}}> No data found</p>
+              ) : (
+                <div>
+                  <h1 style={{ color: "white" }}>{search.toUpperCase()}</h1>
+                  <h2 style={{ color: "white" }}>
+                    {city.temp}
+                    <span>&#8451;</span>
+                  </h2>
+                  <p style={{ color: "white" }}>
+                    Min-temp : {city.temp_min}
+                    <span>&#37;</span> || Max-temp :{city.temp_max}
+                    <span>&#37;</span>
+                  </p>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </div>
-    </>
-  );
-}
+          </div>
+        </>
+      );
+    }
 
-export default Weather;
+    export default Weather;
